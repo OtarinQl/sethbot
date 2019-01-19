26770db0f522a9858cc912074fc3df90c36b41fd
OtarinQl <44878608+OtarinQl@users.noreply.github.com>
2019-01-19 18:53:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Thunderbot v.1.2.1 13/1/2019
+#Thunderbot v.1.2.2 19/1/2019
 import os
 import discord
 from func import twitter, housamo
@@ -21,10 +21,17 @@ async def on_message(msg):
         else:
             x = housamo(msg.content.split(' ')[1],msg.content.split(' ')[2])
         if x[0]:
-            cont = discord.Embed(title=msg.content.split(' ')[1].capitalize(), description=x[1], color=0x00ff00)
-            cont.set_image(url=x[2])
-            cont.set_footer(text=msg.content.split(' ')[1].capitalize(), icon_url=x[3])
-            await bot.send_message(channel, embed=cont)
+            embed = discord.Embed(title=msg.content.split(' ')[1].capitalize(), color=0x00ff00)
+            embed.add_field(name=x[1][0], value=x[1][1], inline=True)
+            embed.add_field(name=x[2][0], value=x[2][1], inline=True)
+            embed.add_field(name=x[3][0], value=x[3][1], inline=True)
+            embed.add_field(name=x[4][0], value=x[4][1], inline=True)
+            embed.add_field(name=x[5][0], value=x[5][1], inline=True)
+            embed.add_field(name=x[6][0], value=x[6][1], inline=True)
+            embed.set_image(url=x[7])
+            embed.set_thumbnail(url=x[8])
+            embed.set_footer(text=msg.content.split(' ')[1].capitalize(), icon_url=x[8])
+            await bot.send_message(channel, embed=embed)
         else:
             await bot.send_message(channel,x[1])
 
