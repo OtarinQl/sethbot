7ba44fceb2b98ceab6e6fc5b9fa60c181e46040d
OtarinQl <44878608+OtarinQl@users.noreply.github.com>
2021-11-28 14:54:13 -0300

    Update e621.py

--- e621.py
+++ e621.py
@@ -1,22 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
+from requests import get
+from random import randint
 
-def search(tags, imageQuantity = 3):
-    tags = tags.replace(' ', '%20')
-    r = requests.get("https://e621.net/post/index/1/" + tags, headers= {'User-Agent' : 'PostmanRuntime/7.15.0'})
-    soup = BeautifulSoup(r.text, "html.parser")
-    thumbnails = soup.find_all(class_="thumb")
-    links = []
-    for x in range(min(imageQuantity, len(thumbnails))):
-        link = thumbnails[x].a
-        links.append(link.get('href'))
-    
-    images = []
-    for link in links:
-        r = requests.get('https://e621.net' + link, headers= {'User-Agent' : 'PostmanRuntime/7.15.0'})
-        soup = BeautifulSoup(r.text, "html.parser")
-        image = soup.find(id='image')
-        if image is not None:
-            images.append(image.get('src'))
+def getImages(tagsList, limit = 1):
+    if not limit.isdigit():
+        tagsList.append(limit)
+        limit = '1'
+    elif int(limit) > 50:
+        raise Exception('Solo pueden buscarse hasta 50 imágenes :face_with_monocle:')
 
-    return images
+    tags = '+'.join(tagsList)
+    URL = f'https://e621.net/posts.json?limit=50&tags={tags}'
+
+    Headers = {'user-agent': 'OtarinOhtome/1.0'}
+    r = get(URL, headers=Headers)
+    if r.status_code != 200:
+        raise Exception('Ha ocurrido un problema al buscar :pensive: probá nuevamente')
+
+    posts = r.json()['posts']
+    if posts:
+        postsData = []
+        for x in range(int(limit)):
+            index = randint(0, 49)
+            post = posts[index]
+            
+            data = {
+                'id':post['id'],
+                'url':post['file']['url'],
+                'author':post['tags']['artist'][0]
+            }
+
+            postsData.append(data)
+    else:
+        return []
+
+    return postsData
